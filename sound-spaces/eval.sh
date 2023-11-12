f600b5370a8248668c4bcb84867648de553902dd
haru425 <hkondoh79@gmail.com>
2023-11-12 19:56:34 +0900

    split eval savi-1st and savi-2nd

--- sound-spaces/eval.sh
+++ sound-spaces/eval.sh
@@ -7,10 +7,10 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate av-nav
 
 ENV="ss1-savi" # ss1-avnav, ss2-avnav, ss1-savi, ss2-savi
-MODEL="savi"   # avnav, avwan, savi
+MODEL="savi-1st"   # avnav, avwan, savi-1st, savi-2nd
 MODEL_NAME="ss1savi-savi"
 DATASET_NAME="mp3d"
-PREV_CKPT_IND=-1
+PREV_CKPT_IND=-1 # savi-2ndなら途中からになるように変更するべき
 TEST_EPISODE_COUNT=100
 
 echo ENV=$ENV
@@ -20,7 +20,21 @@ echo DATASET_NAME=$DATASET_NAME
 echo PREV_CKPT_IND=$PREV_CKPT_IND
 
 
-if [ $ENV = "ss1-savi" && $MODEL = "savi" ]; then
+if [ $ENV = "ss1-savi" && $MODEL = "savi-1st" ]; then
+    python ss_baselines/savi/run.py \
+        --run-type eval \
+        --exp-config ss_baselines/savi/config/semantic_audionav/savi_pretraining.yaml \
+        --model-dir data/models/ss1-savi/$DATASET_NAME/$MODEL_NAME \
+        --prev-ckpt-ind $PREV_CKPT_IND \
+        LOG_FILE data/models/ss1-savi/$DATASET_NAME/$MODEL_NAME/eval.log \
+        TENSORBOARD_DIR data/models/ss1-savi/$DATASET_NAME/$MODEL_NAME/tb_eval \
+        NUM_PROCESSES 10 \
+        CONTINUOUS False \
+        EVAL.SPLIT val \
+        USE_SYNC_VECENV True \
+        TEST_EPISODE_COUNT $TEST_EPISODE_COUNT \
+        RL.DDPPO.pretrained False
+elif [ $ENV = "ss1-savi" && $MODEL = "savi-2nd" ]; then
     python ss_baselines/savi/run.py \
         --run-type eval \
         --exp-config ss_baselines/savi/config/semantic_audionav/savi.yaml \
