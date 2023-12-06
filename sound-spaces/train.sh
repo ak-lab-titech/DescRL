d4da792c6415b9c8dbceee3fe723fac13dd28f36
haru425 <hkondoh79@gmail.com>
2023-12-06 15:21:37 +0900

    change config

--- sound-spaces/train.sh
+++ sound-spaces/train.sh
@@ -3,8 +3,8 @@
 ENV="ss1-savi" # ss1-avnav, ss2-avnav, ss1-savi, ss2-savi
 MODEL="savi-2nd"   # avnav, avwan, savi-1st, savi-2nd, savi-iprl-pretraining
 MODEL_NAME="ss1savi-savi-iprl"
-PRETRAINED_MODEL_NAME="ss1savi-savi"
-CKPT_NUM="88"
+PRETRAINED_MODEL_NAME="ss1savi-savi-1st"
+CKPT_NUM="106"
 
 pwd
 cd ~/av-nav/myss/sound-spaces
