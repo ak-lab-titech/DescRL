80f24720e2a69d41dc9985ed2bc6e8a97b1ac4b4
haru425 <hkondoh79@gmail.com>
2023-12-06 11:49:33 +0900

    fix savi2nd belief_predictor.predictor loading

--- sound-spaces/ss_baselines/savi/ddppo/algo/ddppo_trainer.py
+++ sound-spaces/ss_baselines/savi/ddppo/algo/ddppo_trainer.py
@@ -211,6 +211,13 @@ class DDPPOTrainer(PPOTrainer):
                     if "actor_critic.net.visual_encoder.depth_encoder." in k
                 },
             )
+            self.belief_predictor.predictor.load_state_dict(
+                {
+                    k[len("predictor."):]: v
+                    for k, v in pretrained_state['belief_predictor'].items()
+                    if "predictor." in k
+                }
+            )
 
         if self.config.RL.DDPPO.reset_critic:
             nn.init.orthogonal_(self.actor_critic.critic.fc.weight)
